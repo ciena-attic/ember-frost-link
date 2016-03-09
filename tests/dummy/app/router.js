10bfbe46db411cf9c8c9cdb6be1ee84775d7639f
White, Eric <ewhite@ciena.com>
2016-03-09 14:51:10 -0500

    Change demo to use routing.js

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -5,13 +5,50 @@ var Router = Ember.Router.extend({
   location: config.locationType
 })
 
+var addRoute = function (routeConfig) {
+  if (Ember.isEmpty(routeConfig.items)) {
+    if (routeConfig.path !== undefined && routeConfig.modalName !== undefined) {
+      this.route(routeConfig.id, routeConfig.path, function () {
+        this.modal(routeConfig.modalName, routeConfig.modal)
+      })
+    } else if (routeConfig.path !== undefined) {
+      this.route(routeConfig.id, routeConfig.path)
+    } else if (routeConfig.modalName !== undefined) {
+      this.route(routeConfig.id, function () {
+        this.modal(routeConfig.modalName, routeConfig.modal)
+      })
+    } else {
+      this.route(routeConfig.id)
+    }
+  } else {
+    if (routeConfig.path !== undefined) {
+      this.route(routeConfig.id, routeConfig.path, function () {
+        routeConfig.items.forEach((item) => {
+          addRoute.call(this, item)
+        })
+      })
+    } else {
+      this.route(routeConfig.id, function () {
+        routeConfig.items.forEach((item) => {
+          addRoute.call(this, item)
+        })
+      })
+    }
+
+  }
+}
+
 Router.map(function () {
-  this.route('demo', { path: '/' }, function () {
-    this.route('min')
-    this.route('first', {path: '/first/:first_id'}, function () {
-      this.route('second', {path: '/second/:second_id'})
+  let routerConfig = config.APP.routingConfig
+  routerConfig.forEach((item) => {
+      addRoute.call(this, item)
     })
-  })
+    //  this.route('demo', { path: '/' }, function () {
+    //    this.route('min')
+    //    this.route('first', {path: '/first/:first_id'}, function () {
+    //      this.route('second', {path: '/second/:second_id'})
+    //    })
+    //  })
 })
 
 export default Router
---
+++ tests/dummy/config/routing.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    id: 'demo',
+    alias: 'Demo',
+    type: 'category',
+    route: 'demo',
+    path: {
+      path: '/'
+    },
+    items: [{
+      id: "min",
+      alias: "First",
+      type: "route",
+      route: "demo.min"
+    }, {
+      id: "first",
+      alias: "First",
+      type: "route",
+      route: "demo.min",
+      path: {
+        path: '/first/:first_id'
+      },
+      items: [{
+        id: "second",
+        alias: "Second",
+        type: "route",
+        path: {
+          path: '/second/:second_id'
+        },
+        route: "demo.second"
+    }]
+    }]
+  }
+]

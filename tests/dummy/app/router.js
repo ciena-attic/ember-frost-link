45e722b8d8d89c76a50dd1f634a90f9145c33c64
White, Eric <ewhite@ciena.com>
2016-03-10 10:37:54 -0500

    Use frost-guide-custom-routing addon

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,54 +1,16 @@
 import Ember from 'ember'
 import config from './config/environment'
+import addRoute from 'frost-guide-custom-routing/utils/addRoute'
 
 var Router = Ember.Router.extend({
   location: config.locationType
 })
 
-var addRoute = function (routeConfig) {
-  if (Ember.isEmpty(routeConfig.items)) {
-    if (routeConfig.path !== undefined && routeConfig.modalName !== undefined) {
-      this.route(routeConfig.id, routeConfig.path, function () {
-        this.modal(routeConfig.modalName, routeConfig.modal)
-      })
-    } else if (routeConfig.path !== undefined) {
-      this.route(routeConfig.id, routeConfig.path)
-    } else if (routeConfig.modalName !== undefined) {
-      this.route(routeConfig.id, function () {
-        this.modal(routeConfig.modalName, routeConfig.modal)
-      })
-    } else {
-      this.route(routeConfig.id)
-    }
-  } else {
-    if (routeConfig.path !== undefined) {
-      this.route(routeConfig.id, routeConfig.path, function () {
-        routeConfig.items.forEach((item) => {
-          addRoute.call(this, item)
-        })
-      })
-    } else {
-      this.route(routeConfig.id, function () {
-        routeConfig.items.forEach((item) => {
-          addRoute.call(this, item)
-        })
-      })
-    }
-
-  }
-}
-
 Router.map(function () {
   let routerConfig = config.APP.routingConfig
   routerConfig.forEach((item) => {
-      addRoute.call(this, item)
-    })
-    //  this.route('demo', { path: '/' }, function () {
-    //    this.route('min')
-    //    this.route('first', {path: '/first/:first_id'}, function () {
-    //      this.route('second', {path: '/second/:second_id'})
-    //    })
-    //  })
+    addRoute.call(this, item)
+  })
 })
 
 export default Router
--- tests/dummy/config/routing.js
+++ tests/dummy/config/routing.js
@@ -8,27 +8,27 @@ module.exports = [
       path: '/'
     },
     items: [{
-      id: "min",
-      alias: "First",
-      type: "route",
-      route: "demo.min"
+      id: 'min',
+      alias: 'First',
+      type: 'route',
+      route: 'demo.min'
     }, {
-      id: "first",
-      alias: "First",
-      type: "route",
-      route: "demo.min",
+      id: 'first',
+      alias: 'First',
+      type: 'route',
+      route: 'demo.min',
       path: {
         path: '/first/:first_id'
       },
       items: [{
-        id: "second",
-        alias: "Second",
-        type: "route",
+        id: 'second',
+        alias: 'Second',
+        type: 'route',
         path: {
           path: '/second/:second_id'
         },
-        route: "demo.second"
-    }]
+        route: 'demo.second'
+      }]
     }]
   }
 ]
